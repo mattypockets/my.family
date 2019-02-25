20f9957b16225e199860e0be5ef451862c43ffbd
Matt Gasper <mrgasper27@gmail.com>
2019-02-25 09:38:07 -0600

    Added express-jwt for authentication

---
+++ my-family/routes/api/comments.js
@@ -0,0 +1,10 @@
+const router = require ('express').Router();
+const commentController = require('../../controllers/commentController');
+
+router.route('/:id')
+    .delete(commentController.remove);
+
+router.route('/:postId')
+    .get(commentController.findByPostId);
+
+module.exports = router;
